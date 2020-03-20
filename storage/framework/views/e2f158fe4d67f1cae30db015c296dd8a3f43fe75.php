77803ca37109d00b5a838c36cf3e719c04fbdf48
Matheus Bakaus <matheus.bakaus.bruno@gmail.com>
2020-03-20 14:19:29 -0300

    first commit

---
+++ storage/framework/views/e2f158fe4d67f1cae30db015c296dd8a3f43fe75.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>CRUD DESENVOLVIDO COM LARAVEL</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo asset('assets/style.css')?>" type="text/css">
+</head>
+
+<body id="bckg">
+    <div class="header">
+        <img src="assets/img/unnamed.png" alt="Logo Empresa" height="90" width="100">
+    </div>    
+    <div id='cssmenu'>
+        <ul>
+            <li><a href='#'><span>Home</span></a></li>
+            <li class='active has-sub'><a href='#'><span>Products</span></a>
+            <li><a href='#'><span>About</span></a></li>
+            <li class='last'><a href='#'><span>Contact</span></a></li>
+        </ul>
+    </div>
+
+
+     
+
+
+  
+
+<div class="container">
+
+    <?php echo $__env->yieldContent('content'); ?>
+
+</div>
+
+   
+
+</body>
+
+</html><?php /**PATH /home/bakarib4/Documentos/larcrud/resources/views/products/layout.blade.php ENDPATH**/ ?>
